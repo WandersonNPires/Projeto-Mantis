5298a5b638e79500330e1b511a4f8885e86696dd
Wanderson Neiva <wandersonneivapires@gmail.com>
2024-10-31 18:44:19 -0300

    Configuração WebDrive,Teste inicial de acesso a plataforma mantins e teste de criação de nova tarefa

---
+++ mantis/src/main/java/CriarTarefaTeste.java
@@ -0,0 +1,52 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CriarTarefaTeste {
+
+	@Test
+	public void teste() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+// 		Acessando a plataforma Mantis e inserindo credenciais 
+		
+		driver.get("https://mantis-prova.base2.com.br/login_page.php");
+		WebElement usuario = driver.findElement(By.id("username"));
+		usuario.sendKeys("");
+		WebElement entrarUsuario = driver.findElement(By.xpath("//input[@value = 'Entrar']"));
+		entrarUsuario.click();
+		WebElement senha	= driver.findElement(By.id("password"));
+		senha.sendKeys("");
+		WebElement entrarSenha = driver.findElement(By.xpath("//input[@value = 'Entrar']"));
+		entrarSenha.click();
+	 	
+	
+		driver.get("https://mantis-prova.base2.com.br/bug_report_page.php");
+//		 selecionando os Campos Obrigatorios  
+		Select categoria = new Select(driver.findElement(By.id("category_id")));
+		WebElement resumo = driver.findElement(By.id("summary"));
+		WebElement descricao = driver.findElement(By.id("description"));
+		WebElement botaoCriarTarefa = driver.findElement(By.xpath("//input[@value = 'Criar Nova Tarefa']"));
+		
+// 		Atribundo valores para os campos
+		
+		categoria.selectByValue("2");
+		resumo.sendKeys("Criar Nova Tarefa");
+		descricao.sendKeys("Criação de uma nova tarefa ");
+		botaoCriarTarefa.click();
+		Thread.sleep(5000);
+		
+// Toda vez que realiza a criação de uma tarefa o "ID" da pagina altera, após abrir a visualização da tarefa criada		
+		String urlEsperada ="https://mantis-prova.base2.com.br/view.php";
+		String atualUrl =	driver.getCurrentUrl();
+		System.out.println(atualUrl);
+		Assert.assertTrue(atualUrl.startsWith(urlEsperada));
+		driver.quit();
+	}
+
+}
