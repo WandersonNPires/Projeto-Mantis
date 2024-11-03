1bf3a1b610d0d8381fa91ee3a8009f37a159bb97
Wanderson Neiva <wandersonneivapires@gmail.com>
2024-11-03 15:30:07 -0300

    Ajustando nomeclatura

--- mantis/src/main/java/MinhaVisao.java
+++ mantis/src/main/java/MinhaVisao.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MinhaVisao {
 
 	@Test
-	public void imprimirTarefasCSV() throws InterruptedException {
+	public void visaoLinhadoTempo() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 
